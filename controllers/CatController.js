1bfd19c77689ed0f90e6af4f90d82ee93c7c1410
Aarón Huamán <aaron20hc@gmail.com>
2020-06-11 13:00:17 -0500

    Update CatController (Get...)

---
+++ controllers/CatController.js
@@ -0,0 +1,67 @@
+
+const Gato = require('../models/Gato');
+
+
+const getAllCats = async (req, res) => {
+
+    const result = await Gato.ListarGatos();
+
+    if (!result) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'No se pudo mostrar ningun gato',
+      });
+    }
+
+    res.json({
+      ok: true,
+      gatos: result[0]
+
+    });
+}
+
+const getCatsByRace = async (req, res) => {
+    const body = req.body;
+
+    const { race } = body;
+
+    const result = await Gato.ListarGatosPorRaza(race);
+
+    if (!result) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'No se pudo mostrar ningun gato',
+      });
+    }
+
+    res.json({
+      ok: true,
+      gatos: result[0]
+    });
+}
+
+const getCatsById = async (req, res) => {
+
+    const body = req.body;
+
+    const { catId } = body;
+
+    const result = await Gato.ListarGatoPorId(catId);
+    if (!result) {
+      return res.status(500).json({
+        ok: false,
+        mensaje: 'No se pudo mostrar ningun gato',
+      });
+    }
+
+    res.json({
+      ok: true,
+      gatos: result[0]
+    });
+}
+
+module.exports = {
+    getAllCats,
+    getCatsByRace,
+    getCatsById
+}
--- routes/Gato.js
+++ routes/Gato.js
@@ -2,50 +2,24 @@
 const express = require('express');
 const app = express();
 
-const Gato = require('../models/Gato');
+const CatController = require('../controllers/CatController');
 
-app.get('/gato/listar', async (req, res) => {
-  const result = await Gato.ListarGatos();
-  if (!result) {
-    res.status(500).json({
-      ok: false,
-      mensaje: 'No se pudo mostrar ningun gato',
-    });
-  }
-  res.json({
-    ok: true,
-    gatos: result,
-  });
-});
+app.get(
+  '/gato/listar',
+  [],
+  CatController.getAllCats
+);
 
-app.get('/gato/listar/raza', async (req, res) => {
-  let body = req.body;
-  const result = await Gato.ListarGatosPorRaza(body);
-  if (!result) {
-    res.status(500).json({
-      ok: false,
-      mensaje: 'No se pudo mostrar ningun gato',
-    });
-  }
-  res.json({
-    ok: true,
-    gatos: result,
-  });
-});
+app.get(
+  '/gato/listar/raza',
+  [],
+  CatController.getCatsByRace
+);
 
-app.get('/gato/listar/id', async (req, res) => {
-  let body = req.body;
-  const result = await Gato.ListarGatoPorId(body);
-  if (!result) {
-    res.status(500).json({
-      ok: false,
-      mensaje: 'No se pudo mostrar ningun gato',
-    });
-  }
-  res.json({
-    ok: true,
-    gatos: result,
-  });
-});
+app.get(
+  '/gato/listar/id',
+  [],
+  CatController.getCatsById
+);
 
 module.exports = app;
